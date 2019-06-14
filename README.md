4a7bb2ef7a3ed5aa2b4d2a88d695688feb8d2bba
CharizardFire58 <jd.maceirasg@gmail.com>
2019-06-14 18:21:54 -0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+CP Plays Pokemon (Twitch Plays Pokemon Bot)
+=========== 
+Only support [VisualBOyAdvanvce Emulator](https://www.pokeyplay.com/descargar_archivo.php?archivo=emuladores_gba/VisualBoyAdvance1.7.2.zip) and GB/GBC/GBA Games.
---
+++ TwitchPlays/Output.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwitchPlays
+{
+    public partial class Output : Form
+    {
+        public Output()
+        {
+            InitializeComponent();
+            richTextBox1.BackColor = Color.DarkGray;
+            richTextBox1.ForeColor = Color.White;
+            richTextBox1.BorderStyle = BorderStyle.None;
+            this.BackColor = Color.DarkGray;            
+        }
+
+        public void AddText(string text){
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
